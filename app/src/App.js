3d5fb2d3d15aa5bed6bfac4b3acdf4a89989b6b2
Manuel RUSSO <manuel.russo@worldline.com>
2023-06-26 21:38:38 +0200

    Clean

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
@@ -6,25 +5,11 @@ import Profile from "./Profile";
 
 function App() {
   return (
-
     <div className="App">
-
       <header className="App-header">
         <LoginButton />
         <LogoutButton />
         <Profile />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
