53db2eae87dd11e77eaf99ed9f344e151ed1c3e8
Manuel RUSSO <manuel.russo@worldline.com>
2023-06-26 21:44:54 +0200

    Move buttons into Profile

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import LoginButton from "./LoginButton";
-import LogoutButton from "./LogoutButton";
 import Profile from "./Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Profile/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- app/src/Profile.js
+++ app/src/Profile.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 
 const Profile = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    const {user, isAuthenticated, isLoading} = useAuth0();
 
     if (isLoading) {
         return <div>Loading ...</div>;
     }
 
-    return (
-        isAuthenticated && (
-            <div>
-                <img src={user.picture} alt={user.name} />
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-            </div>
-        )
-    );
+    if (isAuthenticated) {
+        return <div>
+            <img src={user.picture} alt={user.name}/>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <LogoutButton/>
+        </div>
+    }
+
+    return <LoginButton/>
+
 };
 
 export default Profile;
