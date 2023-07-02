50b0bff4236e1d4c4d549b80f18f593d023b8a29
Manuel RUSSO <manuel.russo@worldline.com>
2023-07-02 22:00:04 +0200

    Retrieve h2h odds from api

--- app/src/components/Match/Match.js
+++ app/src/components/Match/Match.js
@@ -2,58 +2,39 @@ import PropTypes from "prop-types";
 import {Component} from "react";
 
 import './Match.css'
-import Outcome from "../Outcome";
 
 class Match extends Component {
 
     static propTypes = {
         match: PropTypes.shape({
             id: PropTypes.string.isRequired,
-            commence_time: PropTypes.instanceOf(Date),
-            home_team: PropTypes.string.isRequired,
-            away_team: PropTypes.string.isRequired,
-            bookmakers: PropTypes.arrayOf(
-                PropTypes.shape({
-                    key: PropTypes.string.isRequired,
-                    markets: PropTypes.arrayOf(
-                        PropTypes.shape({
-                            key: PropTypes.string.isRequired,
-                            outcomes:  PropTypes.arrayOf(
-                                PropTypes.shape({
-                                    name: PropTypes.string.isRequired,
-                                    price: PropTypes.string.isRequired,
-                                })
-                            )
-                        })
-                    )
-                })
-            )
+            begin: PropTypes.instanceOf(Date),
+            home: PropTypes.string.isRequired,
+            away: PropTypes.string.isRequired,
+            h2h: PropTypes.bool.isRequired,
+            h2h_1: PropTypes.string,
+            h2h_X: PropTypes.string,
+            h2h_2: PropTypes.string,
         }).isRequired
     }
 
     render = () => {
         const {match} = this.props
 
-        const unibet = match.bookmakers.find((bookmaker) => bookmaker.key === 'unibet_eu')
-
-        if (unibet) {
-            const h2h = unibet.markets.find((market) => market.key === 'h2h')
-
-            if (h2h) {
-                console.log(h2h)
-                return (
-                    <div className="Match">
-                        <p>{match.commence_time}: {match.home_team} - {match.away_team}</p>
-                        <p>
-                            {h2h.outcomes.map((outcome) => <Outcome outcome={outcome} />)}
-                        </p>
-                    </div>
-                )
-            }
+        if (match.h2h) {
+            return (
+                <div className="Match">
+                    <p>{match.begin}: {match.home} - {match.away}</p>
+                    <p>{match.h2h_1} - {match.h2h_X} - {match.h2h_2}</p>
+                </div>
+            )
+        } else {
+            return (
+                <div className="Match">
+                    <p>{match.begin}: {match.home} - {match.away}</p>
+                </div>
+            )
         }
-
-
-
     }
 
 }
