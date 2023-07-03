49cd0c09f8fc3368dcc54b92384bf1a4a5a264ed
Manuel RUSSO <manuel.russo@worldline.com>
2023-07-03 23:48:59 +0200

    Using material icons for login and logout

--- app/src/LoginButton.js
+++ app/src/LoginButton.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import LoginIcon from '@mui/icons-material/Login';
+
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+    return <LoginIcon onClick={() => loginWithRedirect()}>Log In</LoginIcon>;
 };
 
 export default LoginButton;
